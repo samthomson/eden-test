bdeca6698735b21cb24c3ee4aecaf6872d59a0b1
sam <samt@samt.st>
2023-09-30 08:42:17 +0200

    minor refactoring

--- tx-checker/src/components/TXChecker.tsx
+++ tx-checker/src/components/TXChecker.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import * as Types from "../declarations"
 import useTxAPI from "../hooks/useTxAPI"
 import useStoreTXData from "../hooks/useStoreTXData"
 
@@ -7,21 +8,12 @@ const TXChecker: React.FC = () => {
 	const { storeTXData } = useStoreTXData()
 
 	const [txInput, setTxInput] = React.useState<string>("")
-	const [retrievedTXData, setRetrievedTXData] = React.useState<any | undefined>(
-		undefined
-	)
-
-	const testTx = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-		if (txInput === "") {
-			return
-		}
-		checkTx(txInput)
-	}
+	const [retrievedTXData, setRetrievedTXData] = React.useState<
+		Types.TransactionData | undefined
+	>(undefined)
 
 	// if we get data back from the api - do something with it
 	React.useEffect(() => {
-		console.log("data", txData)
 		// display the tx data - irrespecive of whether we found a broadcast tx or not
 		setRetrievedTXData(txData)
 		// if it did exist on the network, let's store it to our db
@@ -37,6 +29,14 @@ const TXChecker: React.FC = () => {
 
 	const isTransactionDataFound = !!txInput && !isLoading && !!retrievedTXData
 
+	const testTx = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		if (txInput === "") {
+			return
+		}
+		checkTx(txInput)
+	}
+
 	return (
 		<div>
 			<h2>Check a TX</h2>
@@ -51,13 +51,17 @@ const TXChecker: React.FC = () => {
 					size={70}
 				/>
 				&nbsp;
-				<input type="submit" value="Get info" disabled={isLoading} />
+				<input
+					type="submit"
+					value="Get info"
+					disabled={isLoading || txInput === ""}
+				/>
 			</form>
 			<hr />
 			{isLoading && <>loading...</>}
 			{isTransactionDataFound && (
 				<>
-					<h4>{retrievedTXData.txid}</h4>
+					<h4>{retrievedTXData.txId}</h4>
 					Status: {retrievedTXData.status}
 				</>
 			)}
